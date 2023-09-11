683b0546cb78f78d6402bcb5dfc75d7a6c182381
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-11 10:12:57 +0200

    add second page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { SecondPageComponent } from './second-page/second-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SecondPageComponent
   ],
   imports: [
     BrowserModule,
