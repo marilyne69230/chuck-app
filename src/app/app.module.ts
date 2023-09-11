9cbd83c18c23da473e95cbb0518cd1b334800a25
Marilyne Naili <marilyne.lydia@gmail.com>
2023-09-11 11:00:22 +0200

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SecondPageComponent } from './second-page/second-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    SecondPageComponent
+    SecondPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
